1162f804979ddd65e66ae7bdf9b4d809aa668772
Rémi Thebault <remi.thebault@gmail.com>
2026-04-14 15:18:16 +0200

    bump v0.2.0

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ sys.path.insert(0, os.path.abspath("../src"))
 project = "plotive-py"
 copyright = "2026, Rémi THEBAULT"
 author = "Rémi THEBAULT"
-release = "0.1.1"
+release = "0.2.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
