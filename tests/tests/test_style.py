33fe393aca99d8e4a51d040a839c29ea8155a530
Rémi Thebault <remi.thebault@gmail.com>
2026-05-12 21:37:40 +0200

    test style

---
+++ tests/tests/test_style.py
@@ -0,0 +1,52 @@
+import plotive as pv
+from plotive.style import SeriesMarker
+
+from . import *
+
+
+def test_style_line():
+    x, y = [0, 1], [0, 1]
+    plot = pv.Plot(
+        pv.series.Line(x=x, y=y, line=pv.SeriesStroke(width=2)),
+    )
+    assert_fig_eq_ref(fig_small(plot), "style/line-solid")
+
+
+def test_style_line_dashed():
+    x, y = [0, 1], [0, 1]
+    plot = pv.Plot(
+        pv.series.Line(x=x, y=y, line=pv.SeriesStroke(width=2, pattern=[5, 5])),
+    )
+    assert_fig_eq_ref(fig_small(plot), "style/line-dashed")
+
+
+def test_style_line_dotted():
+    x, y = [0, 1], [0, 1]
+    plot = pv.Plot(
+        pv.series.Line(x=x, y=y, line=pv.SeriesStroke(width=2, pattern=[1, 1])),
+    )
+    assert_fig_eq_ref(fig_small(plot), "style/line-dotted")
+
+
+def test_style_line_dash_dotted():
+    x, y = [0, 1], [0, 1]
+    plot = pv.Plot(
+        pv.series.Line(x=x, y=y, line=pv.SeriesStroke(width=2, pattern=[5, 5, 1, 5])),
+    )
+    assert_fig_eq_ref(fig_small(plot), "style/line-dash-dotted")
+
+
+def test_style_line_color():
+    x, y = [0, 1], [0, 1]
+    plot = pv.Plot(
+        pv.series.Line(x=x, y=y, line=pv.SeriesStroke(color="indian red", width=2)),
+    )
+    assert_fig_eq_ref(fig_small(plot), "style/line-color")
+
+
+def test_style_line_marker():
+    x, y = [0, 1], [0, 1]
+    plot = pv.Plot(
+        pv.series.Line(x=x, y=y, marker=SeriesMarker()),
+    )
+    assert_fig_eq_ref(fig_small(plot), "style/line-marker")
